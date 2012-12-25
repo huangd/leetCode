241b0688aa445e8d80c6a96a312fec5e6cea25da
Di HUANG <di.huang@ask.com>
2012-12-25 14:32:07 -0800

    Add one unsolved question

---
+++ src/leetcode/LargestRectangleInHistogram.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+/**
+ * User: huangd
+ * Date: 12/25/12
+ * Time: 2:31 PM
+ */
+public class LargestRectangleInHistogram {
+    //This is very similar to ContainerWithMostWater problem
+}
