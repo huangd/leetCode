1f3b82a073222b0fbc12f6e20dc2fe6d14e97370
Di HUANG <di.huang@ask.com>
2012-12-25 20:02:38 -0800

    Do not know how to solve it.

---
+++ src/leetcode/MaximalRectangle.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * User: huangd
+ * Date: 12/25/12
+ * Time: 7:05 PM
+ */
+public class MaximalRectangle {
+
+    private int maxArea;
+
+    public int maximalRectangle(char[][] matrix) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        maxArea = 0;
+        return maxArea;
+    }
+}
