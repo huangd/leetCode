6bf373aa87b9f40f5b223d5864d769604f96f41c
Di HUANG <di.huang@ask.com>
2012-12-26 22:06:35 -0800

    Add a hint

---
+++ src/leetcode/MinimumPathSum.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * User: huangd
+ * Date: 12/26/12
+ * Time: 8:14 PM
+ */
+public class MinimumPathSum {
+
+    //Dynamic programming
+    //time and space complexity are both O(mn)
+    public int minPathSum(int[][] grid) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        return 0;
+    }
+}
