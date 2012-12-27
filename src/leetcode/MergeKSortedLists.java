6894925cbb98ab5d9dfcef2019f5e7a8623b50dd
Di HUANG <di.huang@ask.com>
2012-12-26 17:50:08 -0800

    Add an idea for this problem

---
+++ src/leetcode/MergeKSortedLists.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+/**
+ * User: huangd
+ * Date: 12/26/12
+ * Time: 5:48 PM
+ */
+public class MergeKSortedLists {
+    //Should use a heap to sort the current top elements from those k list.
+    //In java PriorityQueue is a heap structure.
+}
