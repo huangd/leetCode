59bce61675499b63062e822de922841bc5e576d9
Di HUANG <di.huang@ask.com>
2012-12-29 11:20:05 -0800

    Add a problem

---
+++ src/leetcode/UniqueBinarySearchTrees.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * User: huangd
+ * Date: 12/29/12
+ * Time: 11:05 AM
+ */
+public class UniqueBinarySearchTrees {
+    //TODO: Need to have a solution
+    public int numTrees(int n) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        return 0;
+    }
+}
