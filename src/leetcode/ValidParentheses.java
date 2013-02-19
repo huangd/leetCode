86ead0a2d1bb52b1ebba9c737286cc911d510966
Di HUANG <di.huang@ask.com>
2013-02-18 22:35:07 -0800

    Solve "Valid Parentheses"

---
+++ src/leetcode/ValidParentheses.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+import java.util.Stack;
+
+/**
+ * User: huangd
+ * Date: 2/18/13
+ * Time: 10:19 PM
+ */
+public class ValidParentheses {
+
+    private Stack<String> stack;
+
+    public boolean isValid(String s) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        stack = new Stack<String>();
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c + "");
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    String top = stack.pop();
+                    if (!isMatch(top.charAt(0), c)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    private boolean isMatch(char a, char b) {
+        if (a == '(' && b == ')' || a == '{' && b == '}' || a == '[' && b == ']') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
