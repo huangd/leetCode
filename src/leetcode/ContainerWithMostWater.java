af1be38745b1da5ef8b42efd6cdf9cd438d716e0
Di HUANG <di.huang@ask.com>
2013-02-18 21:20:08 -0800

    Simplify "Container with most water"

--- src/leetcode/ContainerWithMostWater.java
+++ src/leetcode/ContainerWithMostWater.java
@@ -12,12 +12,9 @@ public class ContainerWithMostWater {
         // DO NOT write main() function
         int max = Integer.MIN_VALUE;
         for (int i = 0, j = height.length - 1; i < j; ) {
-            int minHeight = height[i] > height[j] ? height[j] : height[i];
+            int minHeight = Math.min(height[j], height[i]);
             int current = minHeight * (j - i);
-            if (current > max) {
-                max = current;
-            }
-
+            max = Math.max(max, current);
             if (height[i] < height[j]) {
                 ++i;
             } else {
---
+++ src/leetcode/TwoSum.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+/**
+ * User: huangd
+ * Date: 2/18/13
+ * Time: 9:15 PM
+ */
+public class TwoSum {
+}
