704b4efa3e6b15df36c5d62d4a46e0b306222254
Di HUANG <di.huang@ask.com>
2013-03-03 18:35:55 -0800

    Simplify "Merge Intervals"

--- src/leetcode/Interval.java
+++ src/leetcode/Interval.java
@@ -5,7 +5,7 @@ package leetcode;
  * Date: 12/24/12
  * Time: 11:12 PM
  */
-public class Interval implements Comparable<Interval> {
+public class Interval {
     int start;
     int end;
 
@@ -17,31 +17,5 @@ public class Interval implements Comparable<Interval> {
     Interval(int s, int e) {
         start = s;
         end = e;
-    }
-
-    @Override
-    public boolean equals(Object anObject) {
-        if (anObject instanceof Interval) {
-            Interval anInterval = (Interval) anObject;
-            if (anInterval.start == this.start && anInterval.end == this.end) {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
-
-    }
-
-    @Override
-    public int compareTo(Interval interval) {
-        if (this.start > interval.start) {
-            return 1;
-        } else if (this.start == interval.start) {
-            return 0;
-        } else {
-            return -1;
-        }
     }
 }
--- src/leetcode/MergeIntervals.java
+++ src/leetcode/MergeIntervals.java
@@ -2,6 +2,7 @@ package leetcode;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * User: huangd
@@ -10,38 +11,41 @@ import java.util.Collections;
  */
 public class MergeIntervals {
 
-    //Not sure if it is the right solution. Intervals need to implements Comparable interface
     private ArrayList<Interval> intervalArrayList;
+    private Comparator<Interval> ORDER_BY_START = new Comparator<Interval>() {
+        @Override
+        public int compare(Interval interval, Interval interval2) {
+            return new Integer(interval.start).compareTo(new Integer(interval2.start));
+        }
+    };
 
     public ArrayList<Interval> merge(ArrayList<Interval> intervals) {
         // Start typing your Java solution below
         // DO NOT write main() function
         intervalArrayList = new ArrayList<Interval>();
         Interval newInterval = null;
-        Collections.sort(intervals);
+        Collections.sort(intervals, ORDER_BY_START);
         for (int i = 0; i < intervals.size(); ++i) {
             newInterval = process(intervals.get(i), newInterval);
         }
-        process(null, newInterval);
+        if (newInterval != null) {
+            intervalArrayList.add(newInterval);
+        }
         return intervalArrayList;
     }
 
     private Interval process(Interval interval, Interval newInterval) {
-        if (interval == null) {
-            intervalArrayList.add(newInterval);
-            return null;
+        if (newInterval == null) {
+            return interval;
         } else {
-            if (newInterval == null) {
+            if (newInterval.end < interval.start) {
+                intervalArrayList.add(newInterval);
                 return interval;
+            } else if (interval.end < newInterval.start) {
+                intervalArrayList.add(interval);
+                return newInterval;
             } else {
-                if (newInterval.end < interval.start) {
-                    intervalArrayList.add(newInterval);
-                    return interval;
-                } else if (newInterval.end >= interval.start && newInterval.end <= interval.end) {
-                    return new Interval(newInterval.start, interval.end);
-                } else {
-                    return newInterval;
-                }
+                return new Interval(Math.min(interval.start, newInterval.start), Math.max(interval.end, newInterval.end));
             }
         }
     }
