097719de5c6ce5dc530935d3f5924b127491a358
Di HUANG <di.huang@ask.com>
2013-03-09 13:10:20 -0800

    Solve "Pow"

--- src/leetcode/Pow.java
+++ src/leetcode/Pow.java
@@ -9,11 +9,19 @@ public class Pow {
     public double pow(double x, int n) {
         // Start typing your Java solution below
         // DO NOT write main() function
+        if (n == 0) {
+            return 1;
+        }
         boolean isNegative = n >= 0 ? false : true;
         n = Math.abs(n);
         double result = 1;
-        for (int i = 0; i < n; ++i) {
-            result *= x;
+        double currentPow = x;
+        while (n != 0) {
+            if (n % 2 == 1) {
+                result *= currentPow;
+            }
+            currentPow *= currentPow;
+            n /= 2;
         }
         return isNegative ? 1 / result : result;
     }
