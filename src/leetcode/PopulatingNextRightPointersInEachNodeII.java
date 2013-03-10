79b9d42015ea01591fef23b2ae0ccc0c18233168
Di HUANG <di.huang@ask.com>
2013-03-09 23:51:03 -0800

    Simplify "Populating next right pointer in each node" -- constant space

--- src/leetcode/PopulatingNextRightPointersInEachNodeII.java
+++ src/leetcode/PopulatingNextRightPointersInEachNodeII.java
@@ -6,6 +6,7 @@ package leetcode;
  * Time: 11:23 PM
  */
 public class PopulatingNextRightPointersInEachNodeII {
+    //There is still a bug in this solution.
     public void connect(TreeLinkNode root) {
         // Start typing your Java solution below
         // DO NOT write main() function
