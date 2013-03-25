1c62b1fd0e0c22e49634ab9c6ae9bee14f507ee5
Di HUANG <di.huang@ask.com>
2013-03-24 22:25:13 -0700

    Simplify "Search in Rotated Sorted Array"

--- src/leetcode/SearchInRotatedSortedArray.java
+++ src/leetcode/SearchInRotatedSortedArray.java
@@ -6,37 +6,32 @@ package leetcode;
  * Time: 9:05 PM
  */
 public class SearchInRotatedSortedArray {
-    private int[] A;
-    private int target;
 
     public int search(int[] A, int target) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        this.A = A;
-        this.target = target;
-        return search(0, A.length);
-    }
-
-    private int search(int from, int to) {
-        int mid = (to - from) / 2 + from;
-        if (A[mid] == target) {
-            return mid;
-        } else if (from + 1 == to) {
-            return -1;
-        } else {
-            if (A[mid] > A[to - 1]) {
-                if (A[from] <= target && target < A[mid]) {
-                    return search(from, mid);
-                } else {
-                    return search(mid + 1, to);
-                }
+        int from = 0;
+        int to = A.length;
+        while (from < to) {
+            int mid = (from + to) / 2;
+            if (A[mid] == target) {
+                return mid;
             } else {
-                if (A[mid] < target && target <= A[to - 1]) {
-                    return search(mid + 1, to);
+                if (A[mid] > A[to - 1]) {
+                    if (A[from] <= target && target < A[mid]) {
+                        to = mid;
+                    } else {
+                        from = mid + 1;
+                    }
                 } else {
-                    return search(from, mid);
+                    if (A[mid] < target && target <= A[to - 1]) {
+                        from = mid + 1;
+                    } else {
+                        to = mid;
+                    }
                 }
             }
         }
+        return -1;
     }
 }
