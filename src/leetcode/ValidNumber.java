2fb53e432d8706616e29367f618d221e927f70b8
Di HUANG <di.huang@ask.com>
2013-03-24 18:56:59 -0700

    Solve Valid number BF way

--- src/leetcode/ValidNumber.java
+++ src/leetcode/ValidNumber.java
@@ -13,8 +13,60 @@ public class ValidNumber {
     private String matchPattern = "[+-]?(" + numberStart + "|" + dotStart + ")" + eNotation + "?";
 
     public boolean isNumber(String s) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        return s.trim().matches(matchPattern);
+        //return s.trim().matches(matchPattern);
+        s = s.trim();
+        if (s.length() == 0) {
+            return false;
+        }
+        if (s.charAt(0) == '+' || s.charAt(0) == '-') {
+            return isValid(s.substring(1, s.length()));
+        } else {
+            return isValid(s);
+        }
+    }
+
+    public boolean isValid(String s) {
+        boolean hasInteger = false;
+        boolean hasDot = false;
+        boolean hasDecimal = false;
+        boolean hasPow = false;
+        boolean hasSign = false;
+        boolean hasExponent = false;
+        for (char c : s.toCharArray()) {
+            if (c >= '0' && c <= '9') {
+                if (!hasInteger) {
+                    hasInteger = true;
+                } else if (hasDot && !hasPow && !hasDecimal) {
+                    hasDecimal = true;
+                } else if (hasPow && !hasExponent) {
+                    hasExponent = true;
+                }
+            } else if (c == '.') {
+                if (!hasDot && !hasPow) {
+                    hasDot = true;
+                } else {
+                    return false;
+                }
+            } else if (c == 'e') {
+                if (hasInteger && !hasPow) {
+                    hasPow = true;
+                } else {
+                    return false;
+                }
+            } else if (c == '+' || c == '-') {
+                if (hasPow && !hasSign && !hasExponent) {
+                    hasSign = true;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        if ((hasInteger || (hasDot && hasDecimal)) && (hasPow == hasExponent)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
