422c3279b8374b95f28df9666513417c4a3d3c16
Di HUANG <di.huang@ask.com>
2013-04-05 21:15:26 -0700

    Simplify "Minimum Window Substring"

--- src/leetcode/MinimumWindowSubstring.java
+++ src/leetcode/MinimumWindowSubstring.java
@@ -16,36 +16,37 @@ public class MinimumWindowSubstring {
     public String minWindow(String S, String T) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        init(S, T);
+        init(T);
         int min = Integer.MAX_VALUE;
         String minString = "";
         for (int start = 0, end = 0; end < S.length(); ++end) {
             char c = S.charAt(end);
             if (needToFind[c] == 0) {
                 continue;
-            }
-            hasFound[c]++;
-            if (hasFound[c] <= needToFind[c]) {
-                count++;
-            }
-            if (count == T.length()) {
-                while (needToFind[S.charAt(start)] == 0 || needToFind[S.charAt(start)] < hasFound[S.charAt(start)]) {
-                    if (needToFind[S.charAt(start)] < hasFound[S.charAt(start)]) {
-                        --hasFound[S.charAt(start)];
-                    }
-                    ++start;
+            } else {
+                hasFound[c]++;
+                if (hasFound[c] <= needToFind[c]) {
+                    count++;
                 }
-                int current = end - start + 1;
-                if (current < min) {
-                    minString = S.substring(start, end + 1);
-                    min = current;
+                if (count == T.length()) {
+                    while (needToFind[S.charAt(start)] == 0 || needToFind[S.charAt(start)] < hasFound[S.charAt(start)]) {
+                        if (needToFind[S.charAt(start)] < hasFound[S.charAt(start)]) {
+                            --hasFound[S.charAt(start)];
+                        }
+                        ++start;
+                    }
+                    int current = end - start + 1;
+                    if (current < min) {
+                        minString = S.substring(start, end + 1);
+                        min = current;
+                    }
                 }
             }
         }
         return minString;
     }
 
-    private void init(String S, String T) {
+    private void init(String T) {
         count = 0;
         hasFound = new int[256];
         needToFind = new int[256];
