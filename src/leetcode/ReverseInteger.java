faf55aabdadea45f7dfa8546a042b3b29bf4c4ca
Di HUANG <di.huang@ask.com>
2013-04-07 17:42:19 -0700

    Simplify "Reverse integer"

--- src/leetcode/ReverseInteger.java
+++ src/leetcode/ReverseInteger.java
@@ -6,27 +6,29 @@ package leetcode;
  * Time: 8:19 PM
  */
 public class ReverseInteger {
+    //1000 <=> 1
+    //could be integer overflow
     public int reverse(int x) {
         // Start typing your Java solution below
         // DO NOT write main() function
         boolean isNegative = x >= 0 ? false : true;
         x = Math.abs(x);
-        String reverseInt = "";
+        StringBuilder reverseInt = new StringBuilder();
         while (x > 0) {
             int mod = x % 10;
             x /= 10;
             if (mod == 0) {
                 if (reverseInt.length() > 0) {
-                    reverseInt += mod;
+                    reverseInt.append(mod);
                 }
             } else {
-                reverseInt += mod;
+                reverseInt.append(mod);
             }
         }
         if (reverseInt.length() == 0) {
-            reverseInt = "0";
+            reverseInt.append(0);
         }
-        int result = Integer.parseInt(reverseInt);
+        int result = Integer.parseInt(reverseInt.toString());
         return isNegative == true ? result * (-1) : result;
     }
 }
