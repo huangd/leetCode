98c5ae146b2979e90607ac8740cc4eef323c1d96
Di Huang <dhuang@linkedin.com>
2015-09-19 16:46:09 -0700

    SameTree

--- src/leetcode/SameTree.java
+++ src/leetcode/SameTree.java
@@ -7,18 +7,12 @@ package leetcode;
  */
 public class SameTree {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        return isSameSubTree(p, q);
-    }
-
-    public boolean isSameSubTree(TreeNode p, TreeNode q) {
-        if (p != null && q != null) {
-            return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-        } else if (p == null && q == null) {
+        if (p == null && q == null) {
             return true;
-        } else {
-            return false;
         }
+        if (p != null && q != null && p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        return false;
     }
 }
