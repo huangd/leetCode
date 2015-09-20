281eab2214b1268dae0d61f674d3b8475a101ee4
Di Huang <dhuang@linkedin.com>
2015-09-20 09:48:45 -0700

    Lowest Common Ancestor of a Binary Search Tree

--- src/leetcode/LowestCommonAncestorOfABinarySearchTree.java
+++ src/leetcode/LowestCommonAncestorOfABinarySearchTree.java
@@ -1,25 +1,55 @@
 package leetcode;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by huangd on 9/19/15.
  */
 public class LowestCommonAncestorOfABinarySearchTree {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        /**
-         * TODO:
-         * It would be easy if we had a ref from child to parent
-         */
+        List<TreeNode> pathContainP = findNodePath(root, p);
+        for (TreeNode node : pathContainP) {
+            if (containsTargetNode(node, q)) {
+                return node;
+            }
+        }
+        return null;
     }
 
-    private Boolean containNode(TreeNode root, TreeNode p) {
+    /**
+     * It return the TreeNode path from target treeNode to root if it finds the target TreeNode otherwise it returns null
+     */
+    private List<TreeNode> findNodePath(TreeNode root, TreeNode target) {
+        // Reach the leaf node
+        if (root == null) {
+            return null;
+        }
+        // Find the target node
+        if (root.equals(target)) {
+            return new ArrayList(Arrays.asList(root));
+        }
+        List<TreeNode> leftPath = findNodePath(root.left, target);
+        List<TreeNode> rightPath = findNodePath(root.right, target);
+        if (leftPath == null && rightPath == null) {
+            return null;
+        } else {
+            // Find the target node in either left descendants or right descendants
+            List<TreeNode> path = leftPath == null ? rightPath : leftPath;
+            path.add(root);
+            return path;
+        }
+    }
+
+    private boolean containsTargetNode(TreeNode root, TreeNode target) {
         if (root == null) {
             return false;
         }
-
-        if (root == p) {
+        if (root.equals(target)) {
             return true;
         }
-
-        return containNode(root.right, p) || containNode(root.left, p);
+        return containsTargetNode(root.left, target) || containsTargetNode(root.right, target);
     }
 }
--- src/leetcode/TreeNode.java
+++ src/leetcode/TreeNode.java
@@ -10,4 +10,14 @@ public class TreeNode {
     TreeNode left;
     TreeNode right;
     TreeNode(int x) { val = x; }
+
+    @Override public boolean equals(Object object) {
+        if (object instanceof TreeNode) {
+            TreeNode thatTreeNode = ((TreeNode) object);
+            if (thatTreeNode.val == val && thatTreeNode.left == left && thatTreeNode.right == right) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
