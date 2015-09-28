20099ec327f770f36cb96992361145c2c68e6123
Di Huang <dhuang@linkedin.com>
2015-09-27 20:36:33 -0700

    Merge Sorted Array

--- src/leetcode/MergeSortedArray.java
+++ src/leetcode/MergeSortedArray.java
@@ -26,4 +26,27 @@ public class MergeSortedArray {
             --r;
         }
     }
+
+    public void merge2(int[] nums1, int m, int[] nums2, int n) {
+        int i = m + n - 1;
+        int j = m - 1;
+        int k = n - 1;
+        while (j >= 0 && k >= 0) {
+            if (nums1[j] > nums2[k]) {
+                nums1[i] = nums1[j];
+                j--;
+            } else {
+                nums1[i] = nums2[k];
+                k--;
+            }
+            i--;
+        }
+        while (k >= 0) {
+            nums1[i] = nums2[k];
+            i--;
+            k--;
+        }
+    }
 }
+
+
