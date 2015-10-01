19860948263fff825d85bf3dbbb5a6b19982c853
Di Huang <dhuang@linkedin.com>
2015-09-30 22:35:01 -0700

    Simplify solution of Length Of Last Word

--- src/leetcode/LengthOfLastWord.java
+++ src/leetcode/LengthOfLastWord.java
@@ -6,28 +6,9 @@ package leetcode;
  * Time: 2:40 PM
  */
 public class LengthOfLastWord {
-    public int lengthOfLastWord(String s) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        int lastWordLength = 0;
-        boolean isPreSpace = true;
-        boolean isCurrentSpace;
-        for (int i = 0; i < s.length(); ++i) {
-            if (s.charAt(i) == ' ') {
-                isCurrentSpace = true;
-            } else {
-                isCurrentSpace = false;
-            }
 
-            if (!isCurrentSpace) {
-                if (isPreSpace) {
-                    lastWordLength = 1;
-                } else {
-                    lastWordLength += 1;
-                }
-            }
-            isPreSpace = isCurrentSpace;
-        }
-        return lastWordLength;
+    public int lengthOfLastWord(String s) {
+        String[] parts = s.trim().split(" ");
+        return parts[parts.length - 1].length();
     }
 }
