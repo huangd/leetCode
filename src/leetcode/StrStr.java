0936f27d49b7e0ce6ce44c922a5d87673a2ec8ba
Di Huang <dhuang@linkedin.com>
2015-10-03 22:56:00 -0700

    StrStr

---
+++ src/leetcode/StrStr.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+/**
+ * Created by huangd on 10/3/15.
+ */
+public class StrStr {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
