24d1a67cb8eac63e539950fa6e2e939708cf466c
Di Huang <dhuang@linkedin.com>
2015-10-17 15:01:37 -0700

    Binary Tree Inorder Traversal

--- src/leetcode/BinaryTreeInorderTraversal.java
+++ src/leetcode/BinaryTreeInorderTraversal.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 /**
@@ -10,33 +11,18 @@ import java.util.Stack;
  */
 public class BinaryTreeInorderTraversal {
 
-    private ArrayList<Integer> resultList;
-
-    public ArrayList<Integer> inorderTraversal(TreeNode root) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        resultList = new ArrayList<Integer>();
-        inOrder(root);
-        return resultList;
+    List<Integer> res = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        traversal(root);
+        return res;
     }
 
-    private void inOrder(TreeNode treeNode) {
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        TreeNode current = treeNode;
-        boolean isFinished = false;
-        while (!isFinished) {
-            if (current != null) {
-                stack.push(current);
-                current = current.left;
-            } else {
-                if (stack.isEmpty()) {
-                    isFinished = true;
-                } else {
-                    current = stack.pop();
-                    resultList.add(current.val);
-                    current = current.right;
-                }
-            }
+    void traversal(TreeNode node) {
+        if (node == null) {
+            return;
         }
+        inorderTraversal(node.left);
+        res.add(node.val);
+        inorderTraversal(node.right);
     }
 }
