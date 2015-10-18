0646b95aecbc779879687e87eeb911ce3395b5da
Di Huang <dhuang@linkedin.com>
2015-10-18 08:52:47 -0700

    Add cache for it

--- src/leetcode/UniqueBinarySearchTrees.java
+++ src/leetcode/UniqueBinarySearchTrees.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * User: huangd
  * Date: 12/29/12
@@ -7,17 +10,22 @@ package leetcode;
  */
 public class UniqueBinarySearchTrees {
 
+    List<Integer> numTreesCache = new ArrayList<>();
+
     //Recursive is very good here. It used * instead of the regular +
     public int numTrees(int n) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
+        if (numTreesCache.size() > n) {
+            return numTreesCache.get(n);
+        }
         if (n == 0) {
+            numTreesCache.add(n, 1);
             return 1;
         }
         int sum = 0;
         for (int i = 0; i < n; ++i) {
             sum += numTrees(i) * numTrees(n - i - 1);
         }
+        numTreesCache.add(n, sum);
         return sum;
     }
 }
