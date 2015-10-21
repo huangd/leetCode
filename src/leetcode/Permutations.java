1654a62b9490cae2c1a509e59b9cfd46212308b3
Di Huang <dhuang@linkedin.com>
2015-10-20 22:42:15 -0700

    Add backtrack recursive solution

--- src/leetcode/Permutations.java
+++ src/leetcode/Permutations.java
@@ -1,6 +1,8 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * User: huangd
@@ -32,5 +34,38 @@ public class Permutations {
             }
         }
         return newListListInteger;
+    }
+
+    List<List<Integer>> result = new ArrayList<>();
+    public List<List<Integer>> permuteII(int[] nums) {
+        ArrayList<Integer> numsArray = new ArrayList<>();
+        for (int num : nums) {
+            numsArray.add(num);
+        }
+        backtrack(new ArrayList<>(), numsArray);
+        return result;
+    }
+
+    void backtrack(ArrayList<ArrayList<Integer>> partialResult, ArrayList<Integer> nums) {
+        if (nums.size() == 0) {
+            partialResult.forEach(list -> result.add((ArrayList<Integer>) list.clone()));
+            return;
+        }
+        if (partialResult.size() == 0) {
+            partialResult.add(new ArrayList<>());
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            int finalI = i;
+            partialResult.forEach(intList -> intList.add(nums.get(finalI)));
+            ArrayList<Integer> subNums = (ArrayList<Integer>)nums.clone();
+            subNums.remove(i);
+            backtrack(partialResult, subNums);
+            partialResult.forEach(intList -> intList.remove(intList.size() - 1));
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1};
+        System.out.print(new Permutations().permuteII(nums));
     }
 }
