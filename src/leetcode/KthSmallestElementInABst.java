d91323ab9c487376229c65273785aaa141f59724
Di Huang <dhuang@linkedin.com>
2015-10-22 09:07:18 -0700

    KthSmallestElementInABst

---
+++ src/leetcode/KthSmallestElementInABst.java
@@ -0,0 +1,19 @@
+public class KthSmallestElementInABst {
+  public int kthSmallest(TreeNode root, int k) {
+    int countNodeLeft = countNode(root.left);
+    if (countNodeLeft >= k) {
+      return kthSmallest(root.left, k);
+    } else if (countNodeLeft + 1 == k) {
+      return root.val;
+    } else {
+      return kthSmallest(root.right, k - countNodeLeft - 1);
+    }
+  }
+
+  int countNode(TreeNode node) {
+    if (node == null) {
+      return 0;
+    }
+    return countNode(node.left) + 1 + countNode(node.right);
+  }
+}
