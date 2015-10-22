3b38633e783245ce4d5e1b62f97c5b0c7a54e5fc
Di Huang <dhuang@linkedin.com>
2015-10-22 14:51:49 -0700

    Add sudo-algorithm for LinkedListCycleII

---
+++ src/leetcode/LinkedListCycleII.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class LinkedListCycleII {
+    public ListNode detectCycle(ListNode node) {
+        /**
+         * Detect if there is a loop as LinkedListCycleI
+         * Determine the size of the loop
+         * Start again to determine the start of the loop
+         */
+    }
+}
