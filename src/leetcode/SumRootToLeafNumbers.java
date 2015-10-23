b979a3b77930b06bfde24affea0e3b4e338c380f
Di Huang <dhuang@linkedin.com>
2015-10-22 22:57:42 -0700

    SumRootToLeafNumbers

--- src/leetcode/SumRootToLeafNumbers.java
+++ src/leetcode/SumRootToLeafNumbers.java
@@ -10,50 +10,22 @@ import java.util.Set;
  * Time: 11:27 PM
  */
 public class SumRootToLeafNumbers {
-
-    private int sum;
+    int sum = 0;
 
     public int sumNumbers(TreeNode root) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        sum = 0;
-        if (root != null) {
-            ArrayList<String> digitList = new ArrayList<String>();
-            digitList.add(root.val + "");
-            backtrack(digitList, root);
-        }
+        if (root != null) dfs(root, new LinkedList<>());
         return sum;
     }
 
-    private void backtrack(ArrayList<String> digitList, TreeNode treeNode) {
-        if (treeNode.left == null && treeNode.right == null) {
-            sum += digitListToInteger(digitList);
-        } else {
-            Set<TreeNode> candidates = getCandidates(treeNode);
-            for (TreeNode candidate : candidates) {
-                digitList.add(candidate.val + "");
-                backtrack(digitList, candidate);
-                digitList.remove(digitList.size() - 1);
-            }
+    void dfs(TreeNode node, LinkedList<TreeNode> nodePath) {
+        nodePath.add(node);
+        if (node.left != null) dfs(node.left, nodePath);
+        if (node.right != null) dfs(node.right, nodePath);
+        if (node.left == null && node.right == null) {
+            sum += nodePath.stream()
+                .mapToInt(thisNode -> thisNode.val)
+                .reduce(0, (sum, val) -> sum * 10 + val);
         }
-    }
-
-    private Set<TreeNode> getCandidates(TreeNode treeNode) {
-        Set<TreeNode> candidates = new HashSet<TreeNode>();
-        if (treeNode.left != null) {
-            candidates.add(treeNode.left);
-        }
-        if (treeNode.right != null) {
-            candidates.add(treeNode.right);
-        }
-        return candidates;
-    }
-
-    private int digitListToInteger(ArrayList<String> digitList) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (String digit : digitList) {
-            stringBuilder.append(digit);
-        }
-        return Integer.parseInt(stringBuilder.toString());
+        nodePath.removeLast();
     }
 }
