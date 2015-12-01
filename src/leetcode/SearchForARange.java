f7ed17b2626969e809557687fad88333705a562e
Di Huang <dhuang@linkedin.com>
2015-11-30 22:47:45 -0800

    Update SearchForARange

--- src/leetcode/SearchForARange.java
+++ src/leetcode/SearchForARange.java
@@ -6,44 +6,46 @@ package leetcode;
  * Time: 9:45 PM
  */
 public class SearchForARange {
-
-    public int[] searchRange(int[] A, int target) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        int lower = getLowerBound(A, target, 0, A.length);
+    int[] nums;
+    int target;
+    public int[] searchRange(int[] nums, int target) {
+        this.nums = nums;
+        this.target = target;
+        int lower = getLowerBound(0, nums.length);
         if (lower == -1) {
             return new int[]{-1, -1};
+
         } else {
-            int upper = getUpperBound(A, target, 0, A.length);
+            int upper = getUpperBound(0, nums.length);
             return new int[]{lower, upper};
+
         }
     }
 
-    private int getLowerBound(int[] A, int target, int from, int to) {
-        while (from < to) {
-            int mid = (from + to) / 2;
-            if (A[mid] < target) {
-                from = mid + 1;
+    int getLowerBound(int from, int to) {
+        if (from >= to) {
+            if (to < nums.length && nums[to] == target) {
+                return to;
+
             } else {
-                to = mid;
+                return -1;
+
             }
-        }
-        if (from < A.length && A[from] == target) {
-            return from;
-        } else {
-            return -1;
-        }
+
+        } 
+        int mid = (from + to) / 2;
+        if (nums[mid] >= target) return getLowerBound(from, mid);
+        if (nums[mid] < target) return getLowerBound(mid + 1, to);
+        throw new RuntimeException();
+
     }
 
-    private int getUpperBound(int[] A, int target, int from, int to) {
-        while (from < to) {
-            int mid = (from + to) / 2;
-            if (A[mid] > target) {
-                to = mid;
-            } else {
-                from = mid + 1;
-            }
-        }
-        return from - 1;
+    int getUpperBound(int from, int to) {
+        if (from >= to) return from - 1;
+        int mid = (from + to) / 2;
+        if (nums[mid] <= target) return getUpperBound(mid + 1, to);
+        if (nums[mid] > target) return getUpperBound(from, mid);
+        throw new RuntimeException();
+
     }
 }
